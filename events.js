af13f2e1cd31cdb0926b96309ef9cdc3fd23dbe9
Hala Khaled <halakhaled96@hotmail.com>
2022-08-01 04:15:45 +0300

    create server

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
 const EventEmitter = require('events');
-
+const http = require('http');
 class Sales extends EventEmitter {
   constructor() {
     super();
@@ -20,3 +20,20 @@ myEmitter.on('newSale', (stock) => {
   console.log(`There are now ${stock} items left in stock`);
 });
 myEmitter.emit('newSale', 9);
+
+////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received');
+  res.end('request received');
+});
+
+server.on('request', (req, res) => {
+  console.log('another Request ');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('waiting for requests ...');
+});
